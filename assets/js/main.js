49baf7cba8f71c662afc0f35d37c6bb1d75719a1
Oriabure Numerous <noriabure@gmail.com>
2024-06-13 00:00:26 +0100

    Start working on portfolio site

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('send_button');
+
+document.getElementById('contact_form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_3qo1y97';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
